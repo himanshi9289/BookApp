be0aad3db679ad49a95133dc3b9c0646109364ec
Himanshi <himanshisharma9289@gmail.com>
2023-02-17 05:24:04 +0530

    First Commit

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,8 @@
+export default function IndexPage() {
+  return (
+    <div>
+       {/* <Header/> */}
+       Index page here
+    </div>
+  )
+}
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+
+export default function LoginPage() {
+  return (
+    <div className="mt-4">
+      <h1 className="text-4xl text-center">Login</h1>
+      <form action="" className="max-w-md mx-auto border ">
+        <input type="email" name="" placeholder="example@gmail.com" />
+        <input type="password" placeholder="password" />
+        <button>Login here</button>
+      </form>
+    </div>
+  )
+}
