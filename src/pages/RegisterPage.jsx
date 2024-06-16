8d098b8df18dd18f2ec8f64f0ef6d18da44912e5
himanshi9289 <himanshisharma9289@gmail.com>
2024-06-16 17:05:03 +0530

    All Functionalities Added

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
 export default function RegisterPage() {
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  async function registerUser(ev) {
+    ev.preventDefault();
+    try {await axios.post('/register',{
+      name,
+      email,
+      password
+    });
+    alert('Registration Successfully Now you can Login');
+  } catch(e){
+    alert('Registration Failed. Please Try Again ')
+  }
+  }
   return (
-    <div className="mt-4 grow flex items-center justify-around">
-      <div className=" mb-32">
+    <div data-aos='zoom-in' className="mt-4 grow flex items-center justify-around">
+      <div className="mb-32">
       <h1 className="text-4xl text-center mb-4">Register</h1>
-      <form action="" className="max-w-md mx-auto border ">
-        <input type="text" placeholder='Himanshi Sharma' />
-        <input type="email" name="" placeholder="example@gmail.com" />
-        <input type="password" placeholder="password" />
-        <button className="primary">Login here</button>
+      <form action="" className="max-w-md mx-auto border-gray" onSubmit={registerUser}>
+        <input value={name} onChange={ev => setName(ev.target.value)} type="text"  placeholder='Himanshi Sharma' />
+        {
+          
+        }
+        <input value={email} onChange={ev => setEmail(ev.target.value)} type="email" placeholder="example@gmail.com" />
+        <input value={password} onChange={ev => setPassword(ev.target.value)} type="password" placeholder="password" />
+        <button className="primary w-full">Register here</button>
         <div className="text-center py-2 text-gray-500">Already a memeber? 
-        <Link to={'/login'} className="underline text-black" > Sign Up</Link>
+        <Link to={'/login'} className="underline text-black" > Sign In</Link>
          </div>
       </form> 
       </div>
